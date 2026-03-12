d4164822b0c97430dea764660c484381c66d72a7
Isabela Virginio Magella <isabelamagella@gmail.com>
2026-03-12 09:33:45 -0300

    feat: adicionar componente Weather Panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header"
+import { WeatherPanel } from "./components/WeatherPanel"
 
 function App() {
 
   return (
     <>
       <Header />
+      <WeatherPanel />
     </>
   )
 }
---
+++ src/components/WeatherPanel/index.tsx
@@ -0,0 +1,45 @@
+import "./WeatherPanel.css";
+
+export const WeatherPanel = () => {
+  return (
+    <main>
+      <div className="weather-panel">
+        <div className="location-header">
+          <h2 className="current-city">Berlin, Germany</h2>
+          <p className="current-date">Tuesday, Aug 5, 2025</p>
+
+          <div className="weather-info">
+            <img
+              className="weather-icon"
+              src="./images/icon-sunny.webp"
+              alt="Sunny"
+            />
+            <p className="current-temp">68°</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="weather-metrics">
+        <div className="metric-item">
+          <p className="metric-label">Feels Like</p>
+          <p className="metric-value">64°</p>
+        </div>
+
+        <div className="metric-item">
+          <p className="metric-label">Humidity</p>
+          <p className="metric-value">46%</p>
+        </div>
+
+        <div className="metric-item">
+          <p className="metric-label">Wind</p>
+          <p className="metric-value">9 mph</p>
+        </div>
+
+        <div className="metric-item">
+          <p className="metric-label">Precipitation</p>
+          <p className="metric-value">0 in</p>
+        </div>
+      </div>
+    </main>
+  );
+};
